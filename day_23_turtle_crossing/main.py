2ca98cd708c210b3e4fb441360f179961d419c14
BudaBecker <cacocidral@hotmail.com>
2025-03-21 21:13:22 -0300

    bug fixes

--- day_23_turtle_crossing/main.py
+++ day_23_turtle_crossing/main.py
@@ -12,21 +12,33 @@ screen.tracer(0)
 player = Player()
 scoreboard = Scoreboard()
 
+car_list = []
+for _ in range(10):
+    car = CarManager()
+    car_list.append(car)
+
 screen.listen()
 screen.onkeypress(fun=player.move_up, key="Up")
 screen.onkeypress(fun=player.move_down, key="Down")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(0.01)
     screen.update()
     
     if player.ycor() > 280:
         scoreboard.next_level()
         player.restart()
+        for cars in car_list:
+            cars.increment_speed()
         
-    if True: #DISTANCIA CARRO/PLAYER
-        scoreboard.game_over()
-        game_is_on = False
+        
+    for cars in car_list:
+        if cars.distance(player) < 24:
+            scoreboard.game_over()
+            game_is_on = False
+        if cars.xcor() < -280:
+            cars.set_pos()
+        cars.move()
 
 screen.exitonclick()
