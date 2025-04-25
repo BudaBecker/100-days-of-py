3757a39d7ce84e697ee85cf50df4308191841a38
BudaBecker <cacocidral@hotmail.com>
2025-04-24 22:03:09 -0300

    keys

---
+++ day_51/keys.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv("./.venv/.env")
+consumer_key = os.getenv("CONSUMER_KEY")
+consumer_secret = os.getenv("CONSUMER_SECRET")
+access_token = os.getenv("ACCESS_TOKEN")
+access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+bearer_token = os.getenv("BEARER_TOKEN")
